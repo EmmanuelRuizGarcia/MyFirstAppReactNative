a3aa23ce60b80bcedf010ca0aa1be703dee02199
Emmanuel Ruiz Garcia <emmanuel.ruiz.garcia@outlook.com>
2022-09-26 14:00:27 -0500

    add Image Component

--- App.js
+++ App.js
@@ -1,24 +1,26 @@
 import React from "react";
-import { Text, StyleSheet } from "react-native";
+import { Text, StyleSheet, Image } from "react-native";
 
 const App=()=>{
   return(
     <>
       <Text style={stylesPage.styleTitle}>Pagina Login</Text>
+      <Image style={stylesPage.styleImage} resizeMode="contain" source={require("./images/react.png")}/>
     </>
   )
 }
 
 const stylesPage=StyleSheet.create({
+  styleImage:{
+    width:200,
+    height:200
+  },
   styleTitle:{
     fontSize: 24,
     textAlign: "center",
     backgroundColor: "#343a40",
     color: "white",
     fontWeight: "bold",
-    //paddingTop: 20,
-    //paddingBottom: 20
-    //marginTop: 20,
     paddingVertical: 20
   }
 })
