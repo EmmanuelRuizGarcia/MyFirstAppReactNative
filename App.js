44d59fe1c26605e334190ddd7d69a444ae68ea33
Emmanuel Ruiz Garcia <emmanuel.ruiz.garcia@outlook.com>
2022-09-26 12:59:09 -0500

    Edit File App.js

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Text } from "react-native";
+
+const App=()=>{
+  return(
+    <>
+      <Text>Hola Mundo...</Text>
+      <Text>Hola Mundo De Nuevo...</Text>
+    </>
+  )
+}
+
+export default App;
