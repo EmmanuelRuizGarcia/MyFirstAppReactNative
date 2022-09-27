dd085dd18ca9eb6b23f78b15553385495b9a2807
Emmanuel Ruiz Garcia <emmanuel.ruiz.garcia@outlook.com>
2022-09-27 15:45:10 -0500

    se agrego componente FlatList a la pagina TipoLibro

--- src/pages/TipoLibroPage.js
+++ src/pages/TipoLibroPage.js
@@ -1,10 +1,28 @@
-import React from "react";
-import { View, StyleSheet, Text } from "react-native";
+import React, {useState} from "react";
+import { View, StyleSheet, Text, FlatList } from "react-native";
 
 const TipoLibroPage=()=>{
+    const[listatipolibro,setlistatipolibro] = useState([
+        { IIDTIPOLIBRO:1, NOMBRETIPOLIBRO:"Cuento", DESCRIPCION:"Es una historia corta con pocos personajes" },
+        { IIDTIPOLIBRO:2, NOMBRETIPOLIBRO:"Terror", DESCRIPCION:"Historia de mucho miedo" },
+        { IIDTIPOLIBRO:3, NOMBRETIPOLIBRO:"Suspenso", DESCRIPCION:"Nos tiene con el corazón en la mano" },
+        { IIDTIPOLIBRO:4, NOMBRETIPOLIBRO:"Ficcion", DESCRIPCION:"Es irreal" }
+    ])
+
     return(
         <View>
             <Text style={stylesPage.styleTitle}>Tipo Libro</Text>
+            <FlatList 
+                keyExtractor={item=>item.IIDTIPOLIBRO}
+                data={listatipolibro}
+                renderItem={
+                    (tipolibro)=>(
+                          <View>
+                              <Text>{tipolibro.item.NOMBRETIPOLIBRO}</Text>
+                              <Text>{tipolibro.item.DESCRIPCION}</Text>
+                          </View>
+                    )
+                }/>
         </View>
     )
 }
